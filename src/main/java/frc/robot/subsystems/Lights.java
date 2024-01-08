20c4837b9c8763e049a85766d46ccf122f8967ce
messier81porcupine <107228567+messier81porcupine@users.noreply.github.com>
2024-01-07 17:16:52 -0800

    fixing random erros from no auto checker thing
    imports (wth is wpilibj2 anyway)
    added ant math explainer so we dont forget
    METHODES to METHODS
    declared selected and blackOffset
    initialized j in ant loop

--- src/main/java/frc/robot/subsystems/Lights.java
+++ src/main/java/frc/robot/subsystems/Lights.java
@@ -2,14 +2,15 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import edu.wpi.first.wpilibj2.AdressableLED;
-import edu.wpi.first.wpilibj2.AdressableLEDBuffer;
+import edu.wpi.first.wpilibj.AddressableLED;
+import edu.wpi.first.wpilibj.AddressableLEDBuffer;
+
 
 public class Lights extends SubsystemBase {
     private static Lights instance;
 
-    public AdressableLED  m_led = new AddressableLED(9);
-    public AdressableLEDBuffer _ledBuffer = new AddressableLEDBuffer(60);
+    public AddressableLED  m_led = new AddressableLED(9);
+    public AddressableLEDBuffer m_ledBuffer = new AddressableLEDBuffer(60);
 
     private int time = 0;
     private int animationDelay = 10;
@@ -21,6 +22,9 @@ public class Lights extends SubsystemBase {
         return instance;
     }
 
+    private int selected = 0; // selected mode of LEDs
+    private int blackOffset = 0; // k; turns off ever nth LED // is a global variable cause we want the ants to continue thier progress even when switching modes
+
     private void Lights() { //in old code it has no S
         m_led.setLength(m_ledBuffer.getLength());
         m_led.setData(m_ledBuffer);
@@ -28,25 +32,25 @@ public class Lights extends SubsystemBase {
     }
 
     private void update(int selected) {
-        switch (selected)
+        switch (selected){
             case 0: {
                 
                 runAnt(7,2);
                 break;
             }
+        }
     }
 
-    //get and set for light mode
+    // Get and Set LED mode
     public int getSelected() {return selected;} 
     public void SetSelected(int select) { this.selected = select; }
 
-
-    // ------------========+ BEGIN LIGHT METHODES +========------------ \\
+    // ------------========+ BEGIN LIGHT METHODS +========------------ \\
 
     private void runAnt(int groupSize, int offSize) {
         for(int i=0; i > m_ledBuffer.getLength(); i++) {
             if(((i-blackOffset) % groupSize) != 0) {
-                for (int j; j < offSize; j++) {
+                for (int j = 0; j < offSize; j++) {
                     if((i-j) >= 0) {
                         m_ledBuffer.setHSV(i-j,0,0,0);
                     }
@@ -56,12 +60,12 @@ public class Lights extends SubsystemBase {
             }
         }
         m_led.setData(m_ledBuffer);
-        blackoffsetReset();
+        blackOffsetReset(groupSize);
     }
 
-    // ------------=========+ END LIGHT METHODES +=========------------ \\
+    // ------------=========+ END LIGHT METHODS +=========------------ \\
 
-    private void blackoffsetReset(int groupSize) {
+    private void blackOffsetReset(int groupSize) {
         if (blackOffset==groupSize) {
             blackOffset = 0;
         } else { blackOffset++; }
@@ -76,3 +80,36 @@ public class Lights extends SubsystemBase {
     time++;
   }
 }
+
+
+    // ------------========+ ANT MATH EXPLAINER +========------------ \\ 
+/* so i dont forget how it works
+Assuming a groupSize (g) of 5 and a blackOffset (k) of 0 and a blackSize/offsize (b) of 5
+The goal is to turn every 5th (g) LED (i) off 
+To find if i is the 5th LED we subtract k from i to get some number which if it is the 5th LED it should be evenly divisible by 5 (g)
+If (i - k) % g == 0 then the LED at i should be set to black
+If (i - k) % g != 0 then the LED at i should be set to the desired color, in this case red
+
+We first progress through every LED in the strip, every value of i
+The above formula will turn every 5th LED off
+(0 - 0) % 5 == 0 and (5 - 0) % 5 == 0 but (2 - 0) % 5 != 0
+
+
+But we want the black spaces to move to create the animation
+
+To do this we must increment k after we have looped throught the whole LED strip
+This makes it so that every 5th LED is still off but it is now one value of i farther down the strip
+For example: (1 - 1) % 5 == 0 and  (6 - 1) % 5 == 0 mean that LEDs 1 & 6 are black
+but the previously black LED with i value of 5 will be red because (5 - 1) % 5 != 0
+
+**Remember that i of 1 is actually the second LED in the stip as 0 is the first**
+
+We continue this looping through every i value and then incrementing k until k is equal to g
+When k == g we can reset it to be 0 because (5 - 0) % 5 == 0 and (5 - 5) % 5 == 0
+This helps to keep numbers small
+
+Now one final thing, what if we want more than one LED to be turned off
+Simple we create a b value which tells us how many LEDs behind the currently off LED should also be set to black
+It looks like this: if (i - k) % g == 0 then we will set the LEDs from i - b to i as black
+
+*/
