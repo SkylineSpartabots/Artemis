48ee9d5c0ea0c3412fbb081a351d1780e42c8a46
VincentShao32 <yunbo2016@gmail.com>
2024-01-08 21:00:03 -0800

    apply neo conversions, removed ctre configs and changed to neo control modes

--- src/main/java/frc/robot/SwerveModule.java
+++ src/main/java/frc/robot/SwerveModule.java
@@ -14,10 +14,12 @@ import frc.lib.math.NEOConversions;
 import frc.lib.util.CTREModuleState;
 import frc.lib.util.SwerveModuleConstants;
 
-
+import com.ctre.phoenix6.configs.CANcoderConfiguration;
+import com.ctre.phoenix6.configs.MagnetSensorConfigs;
 import com.ctre.phoenix6.hardware.CANcoder;
-
+import com.ctre.phoenix6.signals.AbsoluteSensorRangeValue;
 import com.revrobotics.CANSparkFlex;
+import com.revrobotics.CANSparkBase.ControlType;
 import com.revrobotics.CANSparkBase.IdleMode;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
@@ -60,20 +62,28 @@ public class SwerveModule {
         setSpeed(desiredState, isOpenLoop);
     }
 
+    /**
+     * Sets the speed of the drive motor
+     * @param desiredState Desired set state for PID Controller
+     * @param isOpenLoop   Whether to use open loop or close loop control
+     */
     private void setSpeed(SwerveModuleState desiredState, boolean isOpenLoop){
         if(isOpenLoop){
             double percentOutput = desiredState.speedMetersPerSecond / Constants.SwerveConstants.maxSpeed;
-            mDriveMotor.setControl(dutyCycleRequest.withOutput(percentOutput));
+            mDriveMotor.set(percentOutput);
         }
         else {
-            double velocity = FalconConversions.MPSToFalcon(desiredState.speedMetersPerSecond, Constants.SwerveConstants.wheelCircumference, Constants.SwerveConstants.driveGearRatio);
-            mDriveMotor.setControl(velocityDutyCycle.withVelocity(velocity));
+            double velocity = NEOConversions.MPSToNEO(desiredState.speedMetersPerSecond, Constants.SwerveConstants.wheelCircumference, Constants.SwerveConstants.driveGearRatio);
+            mDriveMotor.getPIDController().setReference(velocity, ControlType.kVelocity);
         }
     }
-
+    /**
+     * Sets the rotation of the angle motor
+     * @param desiredState Desired set state  for PID Controller
+     */
     private void setAngle(SwerveModuleState desiredState){
         Rotation2d angle = (Math.abs(desiredState.speedMetersPerSecond) <= (Constants.SwerveConstants.maxSpeed * 0.01)) ? lastAngle : desiredState.angle; //Prevent rotating module if speed is less then 1%. Prevents Jittering.
-        mAngleMotor.setControl(positionDutyCycleRequest.withPosition(FalconConversions.degreesToFalcon(angle.getDegrees(), Constants.SwerveConstants.angleGearRatio)));
+        mAngleMotor.getPIDController().setReference(NEOConversions.degreesToNEO(angle.getDegrees(), Constants.SwerveConstants.angleGearRatio), ControlType.kPosition);
         lastAngle = angle;
     }
 
@@ -87,18 +97,31 @@ public class SwerveModule {
 
     private void resetToAbsolute(){
         double absolutePosition = NEOConversions.degreesToNEO(getCanCoder().getDegrees() - angleOffset.getDegrees(), Constants.SwerveConstants.angleGearRatio);
-        // check later no idea if this is right
         mAngleMotor.getEncoder().setPosition(absolutePosition);
     }
 
-    private void configAngleEncoder(){    
-        angleEncoder.getConfigurator().apply(CTREConfigs.swerveCanCoderConfig);
+    private void configAngleEncoder(){  
+        /* Swerve CANCoder Configuration */
+        // CANcoder is always initialized to absolute position on boot in Phoenix 6 - https://www.chiefdelphi.com/t/what-kind-of-encoders-are-built-into-the-kraken-motors/447253/7
+
+        MagnetSensorConfigs magnetSensorConfigs = new MagnetSensorConfigs();
+        magnetSensorConfigs.AbsoluteSensorRange = AbsoluteSensorRangeValue.Unsigned_0To1;
+        magnetSensorConfigs.SensorDirection = Constants.SwerveConstants.canCoderInvert;
+        
+        CANcoderConfiguration swerveCanCoderConfig = new CANcoderConfiguration();
+        swerveCanCoderConfig.MagnetSensor = magnetSensorConfigs;  
+        angleEncoder.getConfigurator().apply(swerveCanCoderConfig);
     }   
 
     private void configAngleMotor(){
         mAngleMotor.setSmartCurrentLimit(Constants.SwerveConstants.anglePeakCurrentLimit);
         mAngleMotor.setInverted(Constants.SwerveConstants.angleMotorInvert);
         mAngleMotor.setIdleMode(IdleMode.kBrake);
+
+        mAngleMotor.getPIDController().setP(Constants.SwerveConstants.angleKP);
+        mAngleMotor.getPIDController().setI(Constants.SwerveConstants.angleKI);
+        mAngleMotor.getPIDController().setD(Constants.SwerveConstants.angleKD);
+
         resetToAbsolute();
     }
 
@@ -110,22 +133,28 @@ public class SwerveModule {
         mDriveMotor.setOpenLoopRampRate(Constants.SwerveConstants.openLoopRamp);
         mDriveMotor.setClosedLoopRampRate(Constants.SwerveConstants.closedLoopRamp);
 
+        mDriveMotor.getPIDController().setP(Constants.SwerveConstants.driveKP);
+        mDriveMotor.getPIDController().setI(Constants.SwerveConstants.driveKI);
+        mDriveMotor.getPIDController().setD(Constants.SwerveConstants.driveKD);
+
         mDriveMotor.getEncoder().setPosition(0);
     }
-
+    /**
+     * Gets the current velocity and rotation of the module.
+     * @return A SwerveModuleState containing the current velocity and rotation of the module.
+     */
     public SwerveModuleState getState(){
         return new SwerveModuleState(
-            FalconConversions.falconToMPS(mDriveMotor.getEncoder().getVelocity(), Constants.SwerveConstants.wheelCircumference, Constants.SwerveConstants.driveGearRatio), 
+            NEOConversions.NEOToMPS(mDriveMotor.getEncoder().getVelocity(), Constants.SwerveConstants.wheelCircumference, Constants.SwerveConstants.driveGearRatio), 
 
             getAngle()
         ); 
     }
 
-    // //added by Yanda to try to debug the drifting by comparing actual bus voltage.
-    // public double getDriveBusVoltage(){
-    //     return mDriveMotor.getBusVoltage();
-    // }
-
+    /**
+     * Gets the current distance measured by the wheel and rotation of the module.
+     * @return A SwerveModulePosition containing the current measured distance and rotation of the module.
+     */
     public SwerveModulePosition getPosition(){
         return new SwerveModulePosition(
             FalconConversions.falconToMeters(mDriveMotor.getEncoder().getPosition(), Constants.SwerveConstants.wheelCircumference, Constants.SwerveConstants.driveGearRatio),
