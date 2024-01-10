4eb023ff8455944f1323e09542c991de83d963e2
nat-tri1 <natiey1@outlook.com>
2024-01-09 20:10:41 -0800

    organizing container

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -9,20 +9,39 @@ import frc.robot.commands.Autos;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.ExampleSubsystem;
 import frc.robot.subsystems.ShooterSubsystem;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 public class RobotContainer {
-  private final ShooterSubsystem m_shooterSubsystem;
+  
+  /* Controllers */
+  private final XboxController driver = new XboxController(0);
+  private final XboxController operator = new XboxController(1);
 
-  private final CommandXboxController m_driverController =
-      new CommandXboxController(OperatorConstants.kDriverControllerPort);
+  /* Driver Joysticks (drive control) */
+
+  /* Driver Buttons */
+  private final JoystickButton driverA = new JoystickButton(driver, XboxController.Button.kA.value);
+  
+  /* Operator Buttons */
+
+  /* Operator Sticks */
+
+  /* Subsystems */
+  private final ShooterSubsystem s_shooterSubsystem;
+  private final ExampleSubsystem s_exampleSubsystem = new ExampleSubsystem();
       
   public RobotContainer() {
-    // Configure the trigger bindings
-    m_shooterSubsystem = ShooterSubsystem.getInstance();
+    
+    // Initialize subsystems
+    s_shooterSubsystem = ShooterSubsystem.getInstance();
+    
+    
+    // Configure the button bindings
     configureBindings();
   }
 
@@ -36,13 +55,11 @@ public class RobotContainer {
    * joysticks}.
    */
   private void configureBindings() {
-    m_driverController.b().whileTrue(new InstantCommand(() -> m_shooterSubsystem.setVoltage(6.0)));
+  
   }
-  public void place() {
-
+  
+  public Command getAutonomousCommand() {
+    // An example command will be run in autonomous
+    return Autos.exampleAuto(s_exampleSubsystem);
   }
-  // public Command getAutonomousCommand() {
-  //   // An example command will be run in autonomous
-  //   return Autos.exampleAuto(m_shooterSubsystem);
-  // }
 }
