063fd4ef720211c2e44f6c372894291fe7aac0fa
VincentShao32 <yunbo2016@gmail.com>
2024-01-09 19:43:00 -0800

    robot controller stuff

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,7 +4,9 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
@@ -27,6 +29,9 @@ public class Robot extends TimedRobot {
   @Override
   public void robotInit() {
     m_robotContainer = new RobotContainer();
+    //DriverStation.Alliance a = DriverStation.getAlliance().get();
+    //SmartDashboard.putString("Alliance", a == DriverStation.Alliance.Blue ? "Blue" : a == DriverStation.Alliance.Red ? "Red" : "Other");
+    
   }
 
   /**
@@ -47,7 +52,9 @@ public class Robot extends TimedRobot {
 
   /** This function is called once each time the robot enters Disabled mode. */
   @Override
-  public void disabledInit() {}
+  public void disabledInit() {
+    //m_robotContainer.onRobotDisabled();
+  }
 
   @Override
   public void disabledPeriodic() {}
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,9 +7,15 @@ package frc.robot;
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.Autos;
 import frc.robot.commands.ExampleCommand;
+import frc.robot.commands.TeleopSwerve;
 import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.Swerve;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 /**
@@ -20,14 +26,76 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+  private final ExampleSubsystem exampleSubsystem = new ExampleSubsystem();
+  private final Swerve s_Swerve = Swerve.getInstance();
 
-  // Replace with CommandPS4Controller or CommandJoystick if needed
-  private final CommandXboxController m_driverController =
-      new CommandXboxController(OperatorConstants.kDriverControllerPort);
+  // private final XboxController driver = new XboxController(0);
+  // private final XboxController operator = new XboxController(1);
+  
+  // /* Driver Joysticks (drive control) */
+  // private final int translationAxis = XboxController.Axis.kLeftY.value;
+  // private final int strafeAxis = XboxController.Axis.kLeftX.value;
+  // private final int rotationAxis = XboxController.Axis.kRightX.value;
 
+  // /* Driver Buttons */
+  // private final JoystickButton driverBack = new JoystickButton(driver, XboxController.Button.kBack.value);
+  // private final JoystickButton driverStart = new JoystickButton(driver, XboxController.Button.kStart.value);
+  // private final JoystickButton driverA = new JoystickButton(driver, XboxController.Button.kA.value);
+  // private final JoystickButton driverB = new JoystickButton(driver, XboxController.Button.kB.value);
+  // private final JoystickButton driverX = new JoystickButton(driver, XboxController.Button.kX.value);
+  // private final JoystickButton driverY = new JoystickButton(driver, XboxController.Button.kY.value);
+  // private final JoystickButton driverRightBumper = new JoystickButton(driver,
+  //         XboxController.Button.kRightBumper.value);
+  // private final JoystickButton driverLeftBumper = new JoystickButton(driver, XboxController.Button.kLeftBumper.value);
+
+  // private final Trigger driverDpadUp = new Trigger(() -> driver.getPOV() == 0);
+  // private final Trigger driverDpadRight = new Trigger(() -> driver.getPOV() == 90);
+  // private final Trigger driverDpadDown = new Trigger(() -> driver.getPOV() == 180);
+  // private final Trigger driverDpadLeft = new Trigger(() -> driver.getPOV() == 270);
+  // private final Trigger driverLeftTrigger = new Trigger(() -> {
+  //     return driver.getLeftTriggerAxis() > Constants.triggerDeadzone;
+  // });
+  // private final Trigger driverRightTrigger = new Trigger(() -> {
+  //     return driver.getRightTriggerAxis() > Constants.triggerDeadzone;
+  // });
+
+  // /* Operator Buttons */
+  // private final JoystickButton operatorBack = new JoystickButton(operator, XboxController.Button.kBack.value);
+  // private final JoystickButton operatorStart = new JoystickButton(operator, XboxController.Button.kStart.value);
+  // private final JoystickButton operatorA = new JoystickButton(operator, XboxController.Button.kA.value);
+  // private final JoystickButton operatorB = new JoystickButton(operator, XboxController.Button.kB.value);
+  // private final JoystickButton operatorX = new JoystickButton(operator, XboxController.Button.kX.value);
+  // private final JoystickButton operatorY = new JoystickButton(operator, XboxController.Button.kY.value);
+  // private final JoystickButton operatorRightBumper = new JoystickButton(operator,
+  //         XboxController.Button.kRightBumper.value);
+  // private final JoystickButton operatorLeftBumper = new JoystickButton(operator,
+  //         XboxController.Button.kLeftBumper.value);
+
+  // private final Trigger operatorDpadUp = new Trigger(() -> operator.getPOV() == 0);
+  // private final Trigger operatorDpadRight = new Trigger(() -> operator.getPOV() == 90);
+  // private final Trigger operatorDpadDown = new Trigger(() -> operator.getPOV() == 180);
+  // private final Trigger operatorDpadLeft = new Trigger(() -> operator.getPOV() == 270);
+  // private final Trigger operatorLeftTrigger = new Trigger(() -> {
+  //     return operator.getLeftTriggerAxis() > Constants.triggerDeadzone;
+  // });
+  // private final Trigger operatorRightTrigger = new Trigger(() -> {
+  //     return operator.getRightTriggerAxis() > Constants.triggerDeadzone;
+  // });
+
+  // /* Operator Joysticks */
+  // private final int operatorLeftStick = XboxController.Axis.kLeftY.value;
+  // private final int operatorRightStick = XboxController.Axis.kRightY.value;
+  
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
+    s_Swerve.resetOdometry(new Pose2d());
+    s_Swerve.zeroGyro();
+
+    // s_Swerve.setDefaultCommand(new TeleopSwerve(s_Swerve, 
+    // () -> -driver.getRawAxis(translationAxis),
+    // () -> -driver.getRawAxis(strafeAxis),
+    // () -> -driver.getRawAxis(rotationAxis)));
+
     // Configure the trigger bindings
     configureBindings();
   }
@@ -42,13 +110,10 @@ public class RobotContainer {
    * joysticks}.
    */
   private void configureBindings() {
-    // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
-    new Trigger(m_exampleSubsystem::exampleCondition)
-        .onTrue(new ExampleCommand(m_exampleSubsystem));
+    // driver controls
+    //driverBack.onTrue(new InstantCommand(() -> s_Swerve.resetOdometry(new Pose2d())));
 
-    // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
-    // cancelling on release.
-    m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
+
   }
 
   /**
@@ -58,6 +123,6 @@ public class RobotContainer {
    */
   public Command getAutonomousCommand() {
     // An example command will be run in autonomous
-    return Autos.exampleAuto(m_exampleSubsystem);
+    return Autos.exampleAuto(exampleSubsystem);
   }
 }
