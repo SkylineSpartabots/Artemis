a0837b1bfef9b74ebbbf04641776647ac82e3ee9
Dhruv A <dhruvarora202520@gmail.com>
2024-01-19 19:32:14 -0800

    Changing from Talons to SparkMax

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -8,8 +8,8 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
-import com.ctre.phoenix6.controls.StrictFollower;
-import com.ctre.phoenix6.hardware.TalonFX;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
 
 public class ShooterSubsystem extends SubsystemBase {
   
@@ -20,22 +20,22 @@ public class ShooterSubsystem extends SubsystemBase {
     return instance;
   }
 
-  private TalonFX shooterLeaderM;
-  private TalonFX shooterFollowerM;
+  private CANSparkMax m_shooterLeader;
+  private CANSparkMax m_shooterFollower;
 
   private double voltage;
   
   public ShooterSubsystem() {
-    shooterLeaderM = new TalonFX(Constants.HardwarePorts.shooterLeaderM);
-    shooterFollowerM = new TalonFX(Constants.HardwarePorts.shooterFollowerM);
-    shooterFollowerM.setInverted(true);
-    shooterFollowerM.setControl(new StrictFollower(Constants.HardwarePorts.shooterLeaderM));
+    m_shooterLeader = new CANSparkMax(Constants.HardwarePorts.shooterLeaderM, MotorType.kBrushless);
+    m_shooterFollower = new CANSparkMax(Constants.HardwarePorts.shooterFollowerM, MotorType.kBrushless);
+    m_shooterFollower.setInverted(true);
+    m_shooterFollower.follow(m_shooterLeader);
   }
 
 
   public void setVoltage(double voltage) {
     this.voltage = voltage;
-    shooterLeaderM.setVoltage(voltage);
+    m_shooterLeader.setVoltage(voltage);
   }
 
   public double getVoltageSetpoint() {
