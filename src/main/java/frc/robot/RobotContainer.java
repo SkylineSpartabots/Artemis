6e34df601fb8524ec789cbf61eb3032e44cff85f
nat-tri1 <natiey1@outlook.com>
2024-01-21 15:27:25 -0800

    sendable schooser for auto

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -118,8 +118,4 @@ public class RobotContainer {
    *
    * @return the command to run in autonomous
    */
-  public Command getAutonomousCommand() {
-    // An example command will be run in autonomous
-    return null;
-  }
 }
--- src/main/java/frc/robot/commands/Autos.java
+++ src/main/java/frc/robot/commands/Autos.java
@@ -8,13 +8,55 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 
-public final class Autos {
-  /** Example static factory for an autonomous command. */
-  public static Command exampleAuto() {
+public final class Autos {  
+      
+  private static Command lastCommand;
+  private static Command selectedAuto;
+
+
+  // Return auto selected in Shuffleboard
+  public static Command getAutoCommand(AutoType auto) {
+      switch (auto) {
+      case Test:
+          return selectedAuto = test();
+      case Test2:
+          return selectedAuto = test2();
+      case Test3:
+          return selectedAuto = test3();
+      default:
+          break;
+      }
+      return null;
+  }
+
+  public enum AutoType {
+      Test,
+      Test2,
+      Test3
+  }
+
+  public static Command getSelectedAuto() {
+    return selectedAuto;
+  }
+
+  public static void cancelLastCommand() {
+    lastCommand.cancel();
+  }
+
+
+  private static Command test() {
+    return null;
+  }
+  
+  private static Command test2() {
+    return null;
+  }
+
+  private static Command test3() {
     return null;
   }
 
   private Autos() {
-    throw new UnsupportedOperationException("This is a utility class!");
+      throw new UnsupportedOperationException("This is a utility class!");
   }
 }
