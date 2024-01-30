349a89236e96e45b4a1004c9aeee5658ac48e4ec
Yanda Bao <yandabaodav@gmail.com>
2024-01-29 17:40:24 -0800

    IntakeShooter subsystem method for autointaking

--- src/main/java/frc/robot/subsystems/IntakeShooter.java
+++ src/main/java/frc/robot/subsystems/IntakeShooter.java
@@ -2,9 +2,14 @@ package frc.robot.subsystems;
 
 import javax.sound.midi.MidiEvent;
 
+import com.ctre.phoenix6.signals.ControlModeValue;
 import com.revrobotics.CANSparkFlex;
 import com.revrobotics.CANSparkLowLevel.MotorType;
+import com.revrobotics.ColorMatch;
+import com.revrobotics.ColorSensorV3;
 
+import edu.wpi.first.wpilibj.I2C;
+import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
@@ -15,6 +20,12 @@ public class IntakeShooter extends SubsystemBase {
     private CANSparkFlex mIntakeMotor;
     private CANSparkFlex mLeaderShooter;
     private CANSparkFlex mFollowerShooter;
+
+
+    private static final I2C.Port onboardI2C = I2C.Port.kOnboard;
+    private static ColorSensorV3 m_intakeSensor;
+
+    private static final ColorMatch m_colorMatcherIntake = new ColorMatch();
 
     public static IntakeShooter getInstance(){
         if(mIntakeShooter == null){
@@ -32,11 +43,30 @@ public class IntakeShooter extends SubsystemBase {
 
         mFollowerShooter = new CANSparkFlex(Constants.HardwarePorts.shooterFollowerMotor, MotorType.kBrushless);
         configFollowerMotor();
+
+        m_intakeSensor = new ColorSensorV3(onboardI2C);
+        m_colorMatcherIntake.addColorMatch(new Color(255, 165, 0)); //rgb values for orange, could be inaccurate. YBVS
     }
 
     public void setPercentage(double val){
         mLeaderShooter.set(val);
         mIntakeMotor.set(val);
+    }
+
+    public void setIntake(double val, boolean voltageControl){
+        if(voltageControl){
+            mIntakeMotor.setVoltage(val);
+        } else {
+            mIntakeMotor.set(val);
+        }
+    }
+
+    /**
+     * Equivalent to setIntake(val, false)
+     * @param val is the desired percentage output
+     */
+    public void setIntake(double val){
+        setIntake(val, false);
     }
 
     public void configIntakeMotor(){
@@ -69,6 +99,10 @@ public class IntakeShooter extends SubsystemBase {
         // mLeaderShooter.getPIDController().setP(Constants.SwerveConstants.driveKP);
         // mLeaderShooter.getPIDController().setI(Constants.SwerveConstants.driveKI);
         // mLeaderShooter.getPIDController().setD(Constants.SwerveConstants.driveKD);
+    }
+
+    public boolean hasNote(){
+        return m_intakeSensor.getProximity() >= Constants.noteIntakeDistance;
     }
     
 }
