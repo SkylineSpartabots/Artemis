215632cfc72162a1bce6413ad56ba1a4b8428612
AlbertoKennadiousDaThird <147119726+AlbertoKennadiousDaThird@users.noreply.github.com>
2024-02-21 18:10:47 -0800

    Update Pivot.java

--- src/main/java/frc/robot/subsystems/Pivot.java
+++ src/main/java/frc/robot/subsystems/Pivot.java
@@ -13,7 +13,8 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class Pivot extends SubsystemBase {
-    public static Pivot instance;
+    private static Pivot instance;
+   
     public static Pivot getInstance() {
         if (instance == null) {
             instance = new Pivot();
